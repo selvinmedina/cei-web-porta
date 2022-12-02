920c0dbca1df4e3a1a52552951044ff92c7793ff
Aaron Lemus <aaron.lemus@cmfirstgroup.com>
2022-12-01 20:52:58 -0600

    chartbrew integration

--- cei-web-portal-strapi/config/middlewares.js
+++ cei-web-portal-strapi/config/middlewares.js
@@ -1,6 +1,18 @@
 module.exports = [
   'strapi::errors',
-  'strapi::security',
+  {
+    name: 'strapi::security',
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          'connect-src': ["'self'", 'http:'],
+          
+          upgradeInsecureRequests: null,
+        },
+      },
+    },
+  },
   'strapi::cors',
   'strapi::poweredBy',
   'strapi::logger',
