7fb11285c54387f17f2b02a981e84955b8a91c98
Fernando Rosales <rosalesvegafernando@gmail.com>
2022-12-07 21:01:08 -0800

    Revert "carousel list update"
    This reverts commit 7db5092b2a317d3715106f2f1640ac26b95b269d.

--- cliente/src/pages/LandingPage.js
+++ cliente/src/pages/LandingPage.js
@@ -3,34 +3,19 @@ import { CarouselHome } from '../componentes/CarouselHome'
 import { Home } from '../componentes/Home'
 import { NuestroEquipo } from '../componentes/NuestroEquipo'
 import { QuienesSomos } from '../componentes/QuienesSomos'
-import { API_SERVER ,STRAPI_API,STRAPI_URL} from '../API'
+import { API_SERVER } from '../API'
 
 import '../styles/Home.css'
 
 export const LandingPage = () => {
   const [dataHome, setDataHome] = useState({})
-  const [dataCarousel, setDataCarousel] = useState([])
+
   useEffect(() => {
-    
-    const fetchCarousel = async () =>{
-      const response = await fetch(`${STRAPI_API}casos-exitos?populate=*`)
-      const data = await response.json()
-      console.log("karousel",data)
-      setDataCarousel(data.data.map(d=> {
-        return {
-          id:d.attributes.id,
-          titulo:d.attributes.tittle,
-          descripcion:d.attributes.short_description,
-          imagen: STRAPI_URL.substring(0,STRAPI_URL-1)+ d.attributes.image.data.attributes.url
-        }
-      }))
-    }
     const fetchContactanos = async () => {
       const response = await fetch(`${API_SERVER}inicio`)
       const data = await response.json()
       setDataHome(data)
     }
-    fetchCarousel();
     fetchContactanos()
   }, [])
 
@@ -40,7 +25,7 @@ export const LandingPage = () => {
     <>
       <Home dataHome={dataHome} />
       <CarouselHome
-        carousel={dataCarousel}
+        carousel={carousel}
         titulo_seccion_carousel={titulo_seccion_carousel}
       />
       <QuienesSomos cei_descripcion={cei_descripcion} />
